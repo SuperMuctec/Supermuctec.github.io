441d2d3a26a35070be6255b254cf2d3628d0f6c8
SuperMutec <151155857+SuperMuctec@users.noreply.github.com>
2024-10-10 00:57:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); document.getElementById('form-message').textContent = "Thank you for your message!"; document.getElementById('contact-form').reset();
+    event.preventDefault();
+    document.getElementById('form-message').textContent = "Thank you for your message!";
+    document.getElementById('contact-form').reset();
 });
