6cead855cc501aa949175c939b1476b779b829d5
SuperMutec <151155857+SuperMuctec@users.noreply.github.com>
2024-10-10 00:52:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); document.getElementById('form-message').textContent = "Thank you for your message!"; document.getElementById('contact-form').reset();
+});
