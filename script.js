d93576923bf2f3cd4c053c807161953577f8901f
SuperMutec <151155857+SuperMuctec@users.noreply.github.com>
2024-10-10 15:38:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const contactButton = document.querySelector('nav ul li:nth-child(3)');
-    const dropdown = document.querySelector('.contact-dropdown');
-    const ctaButton = document.querySelector('.cta-button');
+    const contactButton = document.getElementById('contact-button');
     const modal = document.getElementById('myModal');
     const closeButton = document.querySelector('.close-button');
 
-    contactButton.addEventListener('mouseenter', function () {
-        dropdown.style.display = 'block';
-    });
-
-    contactButton.addEventListener('mouseleave', function () {
-        dropdown.style.display = 'none';
-    });
-
-    ctaButton.addEventListener('click', function () {
+    contactButton.addEventListener('click', function (event) {
+        event.preventDefault();
         modal.style.display = 'block';
     });
 
