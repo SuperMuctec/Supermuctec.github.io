3b3d01406835bfddab302819738a1edfa22ecd30
SuperMutec <151155857+SuperMuctec@users.noreply.github.com>
2024-10-10 15:56:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const closeButton = document.querySelector('.close-button');
 
     contactButton.addEventListener('click', function (event) {
-        event.preventDefault();
-        modal.style.display = 'block';
+        event.preventDefault(); // Prevent the default link behavior
+        modal.style.display = 'block'; // Show modal
     });
 
     closeButton.addEventListener('click', function () {
-        modal.style.display = 'none';
+        modal.style.display = 'none'; // Hide modal
     });
 
     window.addEventListener('click', function (event) {
         if (event.target === modal) {
-            modal.style.display = 'none';
+            modal.style.display = 'none'; // Hide modal if clicked outside
         }
     });
 });
