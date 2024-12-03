a0a345805d2bf33684595496f2c511eeba652b29
SuperMutec <supermutec@gmail.com>
2024-12-03 18:00:22 +0530

    Complete
    Completed website by adding Projects and icons

---
+++ Projects/spaghetti.js
@@ -0,0 +1,20 @@
+function darkmode(){
+    
+    const html = document.getElementById("Html")
+
+    if (html.className == "dark"){
+        html.className = ''
+    }
+    else{
+        html.className = "dark"
+    }
+
+    const button = document.getElementById("darkmode-btn")
+
+    if (button.innerHTML === `<img class="w-8" src="../dark.svg">`){
+        button.innerHTML = `<img class="w-8" src="../light.svg">`
+    }
+    else{
+        button.innerHTML = `<img class="w-8" src="../dark.svg">`
+    }
+}
